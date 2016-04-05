61024450f1546b0816ac3d1d8c2f60528f0a0d80
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-04-05 13:58:41 +0000

    Add contacts table and model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,16 +1,3 @@
-class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
-  attribute :nickname,  :captcha  => true
-
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
-  def headers
-    {
-      :subject => "My Contact Form",
-      :to => "your_email@example.org",
-      :from => %("#{name}" <#{email}>)
-    }
-  end
+class Contact < ActiveRecord::Base
+  
 end
