d8bcaf3f56a53a877b967590b5fdbba6dc2c2d2b
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-04-05 14:02:31 +0000

    Add contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,7 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
   end
-
+  
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:error] = nil
-      flash.now[:notice] = 'Gracias por tu mensaje!'
-    else
-      flash.now[:error] = 'Se te olvido algo? Tu mensaje no se ha enviado.'
-      render :new
-    end
   end
 end
