7cba2e802b0729d0b9690f137df116b39372d521
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-04-06 07:39:25 +0000

    Add validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
   end
   
   def create
+      @contact = Contact.new(contact_params)
+  
+  if @contact.save
+    flash[:success] = 'Gracias por tu mensaje.'
+    redirect_to new_contact_path
+  else
+    flash[:danger] = 'Oops! ¿Te falto algo? Trata de nuevo.'
+    redirect_to new_contact_path
   end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
